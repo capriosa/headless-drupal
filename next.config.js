db9339ed2d5130f844f37f5c430504bf09ec0518
M. Droste <m.droste@mdwp.de>
2022-02-02 13:34:40 +0100

    menu on front

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["communitiesdev.devcloud.acquia-sites.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ]
+  },
 }
