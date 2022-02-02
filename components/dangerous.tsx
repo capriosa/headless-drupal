0805e8328b886c62dccb0303b4b5e5e4542b4d77
M. Droste <m.droste@mdwp.de>
2022-02-02 23:17:50 +0100

    grid

--- components/dangerous.tsx
+++ components/dangerous.tsx
@@ -2,8 +2,8 @@ export default function Dangerous(props) {
 
 return (
 <div
-          dangerouslySetInnerHTML={{ __html: props.node }}
-          className="mt-6 font-serif text-xl text-white leading-loose prose"
+          dangerouslySetInnerHTML={{ __html: props.node.processed }}
+          className="mt-6 font-serif text-xl text-white leading-loose"
 />
 )
 }
--- components/node-basic-page.tsx
+++ components/node-basic-page.tsx
@@ -1,13 +1,23 @@
 import Dangerous from "./dangerous"
 
 export function NodeBasicPage({ node, ...props }) {
+  
   return (
     <article {...props}>
       <h1 className="text-6xl text-white my-4 leading-tight">{node.title}</h1>
       {node.body?.processed && (
-        <Dangerous node={node.body.processed} />
+        <Dangerous node={node.body} />
         
       )}
+      
+        <div className="grid grid-cols-3 gap-10 border-t-2 mt-12">
+          { 
+            node.field_columns.map((item,index) => 
+              <Dangerous  key={index} node={item} />
+            )
+          }
+       </div>
+
     </article>
   )
 }
