fb774f933a6eca87fdfbf68e7df0e97ad499edf2
M. Droste <m.droste@mdwp.de>
2022-02-02 17:05:23 +0100

    menu on front

--- components/node-basic-page.tsx
+++ components/node-basic-page.tsx
@@ -1,7 +1,7 @@
 export function NodeBasicPage({ node, ...props }) {
   return (
     <article {...props}>
-      <h1 className="text-6xl font-black mb-4 leading-tight">{node.title}</h1>
+      <h1 className="text-6xl font-black my-4 leading-tight">{node.title}</h1>
       {node.body?.processed && (
         <div
           dangerouslySetInnerHTML={{ __html: node.body?.processed }}
