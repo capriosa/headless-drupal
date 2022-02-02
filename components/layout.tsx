889543b9159b0313bb96574008f41a410e2f0635
M. Droste <m.droste@mdwp.de>
2022-02-02 17:23:35 +0100

    menu on front

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,9 +6,9 @@ import { useRouter } from "next/dist/client/router"
 export function Layout({ children }) {
   
   return (
-    <div className="max-w-screen-md mx-auto px-6">
+    <div className="bg-indigo-600 px-6">
       
-      <main className="container mx-auto py-10">{children} </main>
+      <main className="container mx-auto max-w-screen-lg py-10">{children} </main>
     </div>
   )
 }
