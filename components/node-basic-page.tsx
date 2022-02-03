a3f20da80f597a797a32aff5ed4e874206f78b1f
M. Droste <m.droste@mdwp.de>
2022-02-03 12:54:00 +0100

    image grid

--- components/node-basic-page.tsx
+++ components/node-basic-page.tsx
@@ -1,7 +1,8 @@
 import Dangerous from "./dangerous"
+import Image from "next/image"
 
 export function NodeBasicPage({ node, ...props }) {
-  
+  console.log(node.field_image_columns)
   return (
     <article {...props}>
       <h1 className="text-6xl text-white my-4 leading-tight">{node.title}</h1>
@@ -9,11 +10,11 @@ export function NodeBasicPage({ node, ...props }) {
         <div
         dangerouslySetInnerHTML={{ __html: node.body?.processed }}
         className="mt-6 font-serif text-xl text-white leading-loose"
-/>
+        />
         
       )}
       
-        <div className="grid grid-cols-3 gap-10 border-t-2 mt-24">
+        <div className="grid grid-cols-3 gap-10  mt-6">
           { 
             node.field_columns && (
             node.field_columns.map((item => 
@@ -25,6 +26,48 @@ export function NodeBasicPage({ node, ...props }) {
        )
        }
        </div>
+       {node.field_extra_body?.processed && (
+        <div
+        dangerouslySetInnerHTML={{ __html: node.field_extra_body?.processed }}
+        className="mt-6 font-serif text-xl text-white leading-loose"
+        />
+        
+      )}
+
+        {
+          node.field_image_columns?.uri && (
+          node.field_image_columns[0].uri.url
+          )
+        }
+
+       {
+         
+           node.field_image_columns.map((item =>
+            {node.field_image_columns?.uri && (
+              <>
+              {console.log(item)}
+              {process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${item.uri.url}
+              <figure>
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${item.uri.url}`}
+                  width={360}
+                  height={240}
+                  layout="responsive"
+                  objectFit="cover"
+                  alt={item.resourceIdObjMeta.alt}
+                />
+                {item.resourceIdObjMeta.title && (
+                  <figcaption className="text-sm py-2 text-gray-600 text-center">
+                    {item.resourceIdObjMeta.title}
+                  </figcaption>
+                )}
+              </figure></>
+            )}
+            
+           ))
+         
+       }
+
     </article>
   )
 }
